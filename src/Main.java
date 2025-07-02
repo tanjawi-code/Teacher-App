739559b3e22a887c95d7a09c088d48c79db74944
TANJAWI <ahmedahmed2007tanjawi@gmail.com>
2025-07-02 17:41:05 +0100

    A new copy with object

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,17 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.print("How many students do you have ? : ");
-        int students = input.nextInt();
+        int studentsNumber = input.nextInt();
         input.nextLine();
-        float[][] Grades = new float[students][3];
-        String[] Names = new String[students];
-        int[] Age = new int[students];
-        float[] finalPoints = new float[students];
+
+        Student[] students = new Student[studentsNumber];
 
         System.out.println("\n\n");
-        studentsNames(Names);
+        studentsNames(students);
         System.out.println("\n\n");
-        studentsAge(Age,Names);
+        studentsAge(students);
         System.out.println("\n\n");
-        studentsGrades(Grades,Names,finalPoints);
+        studentsGrades(students);
         System.out.println("\n\n");
 
         int choose ;
@@ -28,15 +26,15 @@ public class Main {
             System.out.println("3 : Showing all the passed student.");
             System.out.println("4 : Showing all the failed student.");
             System.out.println("5 : Exit.");
-            System.out.println("Enter your choice : ");
+            System.out.print("Enter your choice : ");
             choose = input.nextInt();
             System.out.println();
 
             switch (choose){
-                case 1 : details(Names,Age,finalPoints); break;
-                case 2 : searchForStudent(Names,Grades,finalPoints); changing(Names,Grades,finalPoints); break;
-                case 3 : passedStudents(Names,finalPoints); break;
-                case 4 : failedStudents(Names,finalPoints); break;
+                case 1 : details(students); break;
+                case 2 : searchForStudent(students); changing(students); break;
+                case 3 : passedStudents(students); break;
+                case 4 : failedStudents(students); break;
                 case 5 : System.out.println("The program is finished."); break;
                 default :
                     System.out.println("Wrong choice , try again.");
@@ -48,103 +46,103 @@ public class Main {
     }
 
     // This is a function for taking names from the teacher
-    public static void studentsNames(String[] names){
-        for(int x = 0 ; x<names.length ; x++){
+    public static void studentsNames(Student[] students){
+        for(int x = 0 ; x<students.length ; x++){
+            students[x] = new Student();
             System.out.print("Enter student name for number "+(x+1)+" : ");
-            names[x] = input.nextLine().toLowerCase().trim();
+            students[x].setStudentName(input.nextLine().toLowerCase().trim());
         }
     }
 
     // This is a function for taking age from the teacher
-    public static void studentsAge(int[] age , String[] names){
-        for(int y = 0 ; y<age.length ; y++){
-            System.out.print("Enter student age for student "+(names[y])+" : ");
-            age[y] = input.nextInt();
+    public static void studentsAge(Student[] students){
+        for(int y = 0 ; y<students.length ; y++){
+            System.out.print("Enter student age for student "+students[y].getStudentName()+" : ");
+            students[y].setStudentAge(input.nextInt());
         }
     }
 
     // This is a function for taking student's grades from the teacher
-    public static void studentsGrades(float[][] grades , String[] names , float[] finalPoint){
-        for(int a = 0 ; a<grades.length ; a++){
-            for(int b = 0 ; b<grades[a].length ; b++){
-                System.out.println("Enter "+names[a]+"'s point for exam "+(b+1)+" : ");
-                grades[a][b] = input.nextFloat();
-                if(grades[a][b] < 0 || grades[a][b] > 20){
+    public static void studentsGrades(Student[] students){
+        for(int a = 0 ; a<students.length ; a++){
+            for(int b = 0 ; b<3 ; b++){
+                System.out.print("Enter "+students[a].getStudentName()+"'s point for exam "+(b+1)+" : ");
+                students[a].setGrades(input.nextDouble(),b);
+                if(students[a].getStudentGrades(b) < 0 || students[a].getStudentGrades(b) > 20){
                     System.out.println("Enter a grade between 0 and 20.");
                     b--;
                 }
             }
             System.out.println();
         }
-        for(int x = 0 ; x<grades.length ; x++){
-            float sum = 0;
-            for(int y = 0 ; y<grades[x].length ; y++){
-                sum += grades[x][y];
-            }
-            finalPoint[x] = sum/3;
+        for(int x = 0 ; x<students.length ; x++){
+            students[x].calculateGrades();
         }
     }
 
     // This is a function for giving details about every from student
-    public static void details(String[] name , int[] age , float[] finalPoint){
-        for(int x = 0 ; x<name.length ; x++){
-            System.out.printf("Name : "+name[x]+" || Age : "+age[x]+" || Point : %.2f\n ",finalPoint[x]);
+    public static void details(Student[] students){
+        for(int x = 0 ; x<students.length ; x++){
+            students[x].studentDetails();
         }
+        System.out.println();
     }
 
     // This is a function for showing passed students
-    public static void passedStudents(String[] name , float[] points){
-        float passed = 10 , passedStudent = 0;
-        for(int a = 0 ; a<points.length ; a++) {
-            if (passed <= points[a]) {
-                System.out.printf(name[a]+ " : %.2f\n", points[a]);
+    public static void passedStudents(Student[] students){
+        double passed = 10 ;
+        int passedStudent = 0;
+        for(int a = 0 ; a<students.length ; a++) {
+            if (passed <= students[a].getStudentPoint()) {
+                System.out.printf(students[a].getStudentName()+ " : %.2f\n", students[a].getStudentPoint());
                 passedStudent++;
             }
 
         }
         if(passedStudent == 0){
             System.out.println("No passed students.");
-            System.out.println("\n");
+            System.out.println();
         }
         else{
-            System.out.println("\n");
+            System.out.println();
         }
     }
 
     // This is a function for showing failed students
-    public static void failedStudents(String[] name , float[] points){
-        float failed = 9.99f , failedStudent = 0;
-        for(int a = 0 ; a<points.length ; a++){
-            if(failed >= points[a]){
-                System.out.printf(name[a]+" : %.2f\n",points[a]);
+    public static void failedStudents(Student[] students){
+        double failed = 9.99f ;
+        int failedStudent = 0;
+        for(int a = 0 ; a<students.length ; a++){
+            if(failed >= students[a].getStudentPoint()){
+                System.out.printf(students[a].getStudentName()+" : %.2f\n",students[a].getStudentPoint());
                 failedStudent++;
             }
         }
         if(failedStudent == 0){
             System.out.println("No failed students.");
-            System.out.println("\n");
+            System.out.println();
         }
         else{
-            System.out.println("\n");
+            System.out.println();
         }
     }
 
     // This function is about finding a student
-    public static void searchForStudent(String[] name , float[][] Grades , float[] point){
-        System.out.print("Enter the student you want to change : ");
+    public static void searchForStudent(Student[] students){
+        System.out.print("Enter the student name you want to change : ");
         input.nextLine();
         String student = input.nextLine().toLowerCase().trim();
         boolean found = false ;
 
-        for(int x = 0 ; x<name.length ; x++){
-            if(student.equals(name[x])){
-                System.out.println("The student "+name[x]+" is found");
-                for(int y= 0 ; y<Grades[x].length ; y++){
-                    System.out.printf("Exam : "+(y+1)+" is : %.2f\n",Grades[x][y]);
+        for(int x = 0 ; x<students.length ; x++){
+            if(student.equals(students[x].getStudentName())){
+                System.out.println("The student "+students[x].getStudentName()+" is found");
+                for(int y= 0 ; y<3 ; y++){
+                    System.out.printf("Exam : "+(y+1)+" is : %.2f\n",students[x].getStudentGrades(y));
                 }
-                System.out.printf("His/Her point is : %.2f\n",point[x]);
+                System.out.printf("His/Her point is : %.2f\n",students[x].getStudentPoint());
                 found = true ;
-                index = x ;
+                index = x;
                 System.out.println("\n\n");
             }
         }
@@ -154,51 +152,48 @@ public class Main {
         }
     }
 
-    public static void changing(String[] names , float[][] grades , float[]Point){
+    public static void changing(Student[] students){
         if(index!=-1){
-            for(int a = 0 ; a<grades[index].length ; a++){
-                float add , subtract ;
-                System.out.printf("Do you want to change "+(a+1)+" exam : %.2f\n",grades[index][a]);
-                System.out.print("Choose 1 to continue or any number to skip : ");
+            for(int a =0 ; a<3; a++){
+                double add , subtract ;
+                System.out.printf("Do you want to change "+(a+1)+" exam : %.2f\n",students[index].getStudentGrades(a));
+                System.out.print("Choose 1 to continue or any number to skip and see the next grade : ");
                 int choose = input.nextInt();
                 if(choose!=1){
                     continue;
                 }
-                System.out.print("1 : + || 2 : -");
+                System.out.println("1 : + || 2 : -");
                 System.out.print("Choose : ");
                 int operator = input.nextInt();
                 if(operator==1){
                     System.out.print("How much do you want to add : ");
-                    add = input.nextFloat();
+                    add = input.nextDouble();
                     if(add == 0 || add > 20) {
                         System.out.print("The number dose not contains the condition.");
                     }
                     else{
-                        grades[index][a] += add;
+                        students[index].addToTheGrade(a,add);
                     }
                 }
                 else if (operator==2){
                     System.out.print("How much do you want to subtract : ");
                     subtract = input.nextFloat();
-                    if(subtract == 0 || subtract > grades[index][a]) {
+                    if(subtract == 0 || subtract > students[index].getStudentGrades(a)) {
                         System.out.print("The number dose not contains the condition.");
                     }
                     else{
-                        grades[index][a] -= subtract;
+                        students[index].subtractFromTheGrade(a,subtract);
                     }
                 }
                 else{
                     System.out.println("Wrong choice.");
                 }
-                if(grades[index][a] > 20){
-                    grades[index][a] = 20;
-                }
-                float sum = 0;
-                for(int b = 0 ; b<grades[index].length ; b++){
-                    sum += grades[index][b];
-                }
-                Point[index] = sum/grades[index].length ;
-                System.out.printf("The new point of the student "+names[index]+" is : %.2f\n",Point[index]);
+
+                students[index].calculateGrades();
+
+                System.out.printf("The new point of the student "+students[index].getStudentName()+
+                        " is : %.2f\n",students[index].getStudentPoint());
+                System.out.println();
             }
         }
     }
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,48 @@
 public class Student{
+
+    private String studentName;
+    private int studentAge;
+    private double[] studentGrades = new double[3];
+    private double studentPoint;
+
+    public void setStudentName(String name){
+        this.studentName = name;
+    }
+    public String getStudentName(){
+        return studentName;
+    }
+    public void setStudentAge(int Age) {
+        this.studentAge = Age;
+    }
+    public void setGrades(double grade , int index){
+        this.studentGrades[index] = grade;
+   }
+
+    public void calculateGrades(){
+        double sum = 0;
+        for(int x = 0 ; x<studentGrades.length ; x++){
+            sum += studentGrades[x];
+        }
+        this.studentPoint = sum/studentGrades.length;
+   }
+    public double getStudentGrades(int index){
+        return this.studentGrades[index];
+   }
+    public void addToTheGrade(int index , double grade){
+        this.studentGrades[index] = this.studentGrades[index] + grade ;
+        if(this.studentGrades[index] > 20){
+            this.studentGrades[index] = 20;
+        }
+   }
+    public void subtractFromTheGrade(int index , double grade){
+        this.studentGrades[index] = this.studentGrades[index] - grade ;
+    }
+
+    public double getStudentPoint(){
+        return studentPoint;
+    }
+
+    public void studentDetails(){
+        System.out.printf("Name : "+studentName+" || Age : "+studentAge+" || Point : %.2f\n ", studentPoint);
+    }
 }
